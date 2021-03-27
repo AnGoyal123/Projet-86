4d80d04dc6deb8aa2bd505be4846ab93591a4e3f
AnGoyal123 <72863215+AnGoyal123@users.noreply.github.com>
2021-03-27 16:35:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+var player_x = 10;
+var player_y = 10;
+
+var block_image_hieght = 30;
+var block_image_width = 30;
+
+var player_object = "";
+var block_image_object = "";
+
+function player_update(){
+    fabric.Image.fromURL("player.png" , function(Img){
+        player_object = Img;
+
+        player_object.scaleToHeight(140);
+        player_object.scaleToWidth(150);
+
+        player_object.set({
+            top:player_y,
+            left:player_x
+        })
+        canvas.add(player_object);
+
+    })
+}
+
+function new_Image(get_Image){
+    fabric.Image.fromURL(get_Image , function(Img){
+        block_image_object = Img;
+
+        block_image_object.scaleToHeight(block_image_hieght);
+        block_image_object.scaleToWidth(block_image_width);
+
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        })
+        canvas.add(block_image_object);
+        
+    })
+}
